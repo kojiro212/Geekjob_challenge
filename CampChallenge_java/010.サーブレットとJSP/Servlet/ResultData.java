ed4968a1b6cf9ddfb5d8b5c2c52521fe01b9de93
Matsui Koji <ftll9212@gmail.com>
2017-11-22 11:46:29 +0900

    java10を提出します。

---
+++ CampChallenge_java/010.サーブレットとJSP/Servlet/ResultData.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.Serializable;
+import java.util.Date;
+/**
+ *
+ * @author matsuikoji
+ */
+public class ResultData implements Serializable {
+        private Date d;
+        private String luck;
+        
+        public ResultData() {}
+        
+        
+        public Date getD(){
+            return d;
+        }
+        public void setD(Date d){
+            this.d = d;
+        }
+          public String getLuck(){
+              return luck;
+          }
+          public void setLuck(String n){
+              this.luck = n;
+          }
+}
+       
