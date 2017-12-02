c9db39a925f35a94a4e4864289d83622d2816cc2
Matsui Koji <ftll9212@gmail.com>
2017-12-02 22:25:56 +0900

    ファイルを提出します。

---
+++ CampChallenge_java/015.標準クラス/methodclass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+import java.io.*;
+/**
+ *
+ * @author matsuikoji
+ */
+public class methodclass {
+
+    public static void main(String[] args) throws IOException {
+
+    File fp =new File("test.txt");
+    FileWriter fw=new FileWriter(fp);
+    fw.write("松井宏司");
+    fw.close();
+    System.out.print("完了");
+    }
+}
